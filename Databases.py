9a723730642cb1e9d3192e4f82be2fdf37db1a02
pmcderm <38894163+pmcderm@users.noreply.github.com>
2022-12-10 16:43:17 +0100

    moved creation queries to separate file, continued inserting into tables

--- Databases.py
+++ Databases.py
@@ -1,21 +1,38 @@
 import mysql.connector
 import random
-import string
+import table_creation_strings
 
-female_names = ["Emily", "Emma", "Madison", "Abigail", "Olivia", "Isabella", "Sophia", "Ava", "Charlotte", "Mia", "Amelia", "Evelyn", 
-"Abby", "Ellie", "Harper", "Emily", "Avery", "Ella", "Grace", "Sofia", "Chloe", "Zoe", "Victoria", "Aubrey", "Scarlett", "Hannah", 
-"Lily", "Aria", "Layla", "Riley", "Zoey", "Samantha", "Nora", "Leah", "Audrey", "Brooklyn", "Bella", "Violet", "Claire", "Savannah", 
-"Allison", "Sydney", "Skylar", "Camila", "Natalie", "Solange"]
+# make mysql connection
+my_connection = mysql.connector.connect(
+  host="localhost",
+  user="pmcderm27",
+  database="DatabasesProjectTSE"
+)
 
-male_names = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", 
-"Matthew", "George", "Donald", "Anthony", "Paul", "Mark", "Edward", "Steven", "Kenneth", "Andrew", "Brian", "Joshua", "Kevin", "Ronald",
-"Timothy", "Jason", "Jeffrey", "Frank", "Gary", "Ryan", "Nicholas", "Eric", "Stephen", "Jacob", "Larry", "Jonathan", "Scott", "Raymond", 
-"Justin", "Brandon", "Gregory", "Samuel", "Benjamin"]
+# check connection
+print(my_connection)
 
-surnames = ["Martin", "Bernard", "Dubois", "Thomas", "Robert", "Richard", "Petit", "Durand", "Leroy", "Moreau", "Simon", "Laurent", 
-"Lefebvre", "Michel", "Garcia", "David", "Bertrand", "Roux", "Vincent", "Fournier", "Morel", "Girard", "Andre", "Leclerc", "Mercier", "Dupont", 
-"Lambert", "Bonnet", "Francois", "Martinez", "Legrand", "Garnier", "Faure", "Rousseau", "Blanc", "Guerin", "Muller", "Henry", "Roussel", "Nicolas", 
-"Perrin", "Morin", "Mathieu", "Clement"]
+# create cursor
+my_cursor = my_connection.cursor(buffered=True)
+
+# create all the mysql tables
+my_cursor.execute(table_creation_strings.create_activity)
+my_cursor.execute(table_creation_strings.create_contact)
+my_cursor.execute(table_creation_strings.create_nurse)
+my_cursor.execute(table_creation_strings.create_medication)
+my_cursor.execute(table_creation_strings.create_inhabitant)
+my_cursor.execute(table_creation_strings.create_session)
+my_cursor.execute(table_creation_strings.create_userSN)
+my_cursor.execute(table_creation_strings.create_comments)
+my_cursor.execute(table_creation_strings.create_visit)
+my_cursor.execute(table_creation_strings.create_participation)
+my_cursor.execute(table_creation_strings.create_declaring)
+my_cursor.execute(table_creation_strings.create_recommending)
+my_cursor.execute(table_creation_strings.create_following)
+my_cursor.execute(table_creation_strings.create_liking)
+my_cursor.execute(table_creation_strings.create_prescribing)
+
+###Fill activities table
 
 # Define a list of common activities
 activities = ["Hiking", "Biking", "Swimming", "Yoga", "Pilates", "Running", "Walking", "Tennis", "Golf", "Dancing", "Singing", "Acting", "Painting", 
@@ -35,202 +52,69 @@ for activity in activities:
     # Store the activity information in the dictionary
     activity_dict[activity] = {"difficulty": difficulty, "price": price}
 
-
-my_connection = mysql.connector.connect(
-  host="localhost",
-  user="pmcderm27",
-  database="DatabasesProjectTSE"
-)
-
-print(my_connection)
-
-my_cursor = my_connection.cursor(buffered=True)
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS ContactPerson(
-      ContactId INT AUTO_INCREMENT PRIMARY KEY,
-      ContactName VARCHAR(50),
-      ContactLastName VARCHAR(50),
-      ContactPhoneNumber VARCHAR(50)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS SkillActivity(
-        ActivityName VARCHAR(50),
-        Difficulty INT,
-        Price DECIMAL(4,2),
-        PRIMARY KEY(ActivityName)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Nurse(
-      NurseId INT AUTO_INCREMENT PRIMARY KEY,
-      Surname VARCHAR(50),
-      Name VARCHAR(50),
-      NurseAddress VARCHAR(50),
-      PhoneNb INT
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Medication(
-      NameM VARCHAR(50),
-      ActiveSubstance VARCHAR(50),
-      PRIMARY KEY(NameM)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Inhabitant(
-      InHabitantId INT AUTO_INCREMENT PRIMARY KEY,
-      LastName VARCHAR(50),
-      FirstName VARCHAR(50),
-      Category CHAR(50),
-      Address VARCHAR(50),
-      PhoneNumber VARCHAR(50),
-      Gender BINARY(50),
-      NurseId_Follows INT,
-      ContactId INT,
-      FOREIGN KEY(NurseId_Follows) REFERENCES Nurse(NurseId),
-      FOREIGN KEY(ContactId) REFERENCES ContactPerson(ContactId)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Session(
-      SessionId INT AUTO_INCREMENT PRIMARY KEY,
-      Dates DATE,
-      StartingTime TIME,
-      EndingTime TIME,
-      ActivityName VARCHAR(50) NOT NULL,
-      InHabitantId INT NOT NULL,
-      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
-      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS UserSN(
-      LogIn VARCHAR(50),
-      Password VARCHAR(50),
-      Pseudo VARCHAR(50),
-      Email VARCHAR(50),
-      InHabitantId INT NOT NULL,
-      PRIMARY KEY(LogIn),
-      UNIQUE(InHabitantId),
-      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Comments(
-      CommentID INT AUTO_INCREMENT PRIMARY KEY,
-      DateComment DATETIME,
-      Content VARCHAR(140),
-      CommentID_isCommentedBy INT,
-      LogIn VARCHAR(50) NOT NULL,
-      FOREIGN KEY(CommentID_isCommentedBy) REFERENCES Comments(CommentID),
-      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Visit(
-      VisitId INT AUTO_INCREMENT PRIMARY KEY,
-      DateVisit DATETIME,
-      Duration INT,
-      Reason CHAR(50),
-      TextualRemark VARCHAR(200),
-      BloodPressureSystolic INT,
-      BloodPressureDiastolic INT,
-      SugarLevel INT,
-      InHabitantId INT NOT NULL,
-      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Participation(
-      InHabitantId INT,
-      SessionId INT,
-      Rating INT,
-      PRIMARY KEY(InHabitantId, SessionId),
-      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId),
-      FOREIGN KEY(SessionId) REFERENCES Session(SessionId)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Declaring(
-      ActivityName VARCHAR(50),
-      LogIn VARCHAR(50),
-      LevelD VARCHAR(50),
-      PRIMARY KEY(ActivityName, LogIn),
-      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
-      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn)
-    );
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Recommending(
-      ActivityName VARCHAR(50),
-      LogIn_is_recommended_by VARCHAR(50),
-      LogIn_recommends VARCHAR(50),
-      LevelR VARCHAR(50),
-      PRIMARY KEY(ActivityName, LogIn_is_recommended_by, LogIn_recommends),
-      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
-      FOREIGN KEY(LogIn_is_recommended_by) REFERENCES UserSN(LogIn),
-      FOREIGN KEY(LogIn_recommends) REFERENCES UserSN(LogIn)
-);
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Following(
-      LogIn_Is_Followed_by_ VARCHAR(50),
-      LogIn_Follows VARCHAR(50),
-      PRIMARY KEY(LogIn_Is_Followed_by_, LogIn_Follows),
-      FOREIGN KEY(LogIn_Is_Followed_by_) REFERENCES UserSN(LogIn),
-      FOREIGN KEY(LogIn_Follows) REFERENCES UserSN(LogIn)
-);
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Liking(
-      LogIn VARCHAR(50),
-      CommentID INT,
-      PRIMARY KEY(LogIn, CommentID),
-      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn),
-      FOREIGN KEY(CommentID) REFERENCES Comments(CommentID)
-);
-""")
-
-my_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Prescribing(
-      InHabitantId INT,
-      NameM VARCHAR(50),
-      Startingtime DATE,
-      Duration INT,
-      PRIMARY KEY(InHabitantId, NameM),
-      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId),
-      FOREIGN KEY(NameM) REFERENCES Medication(NameM)
-);
-""")
-
-# Fill activities table
-# Iterate over the items in the activities dictionary
+# iterate over the items in the activities dictionary
 for name, info in activity_dict.items():
-    # Get the difficulty and price for the activity
+    # get the difficulty and price for the activity
     difficulty = info["difficulty"]
     price = info["price"]
-    print(name, price, difficulty)
-    # Use the INSERT statement to add the activity data to the database
+    # use the INSERT statement to add the activity data to the database
     my_cursor.execute("INSERT IGNORE INTO SkillActivity (ActivityName, Difficulty, Price) VALUES (%s, %s, %s)", (name, difficulty, price))
 
+# we're going to create the names by randomly selecting from a list of first and last names, so we make lists of names to choose from
+female_names = ["Emily", "Emma", "Madison", "Abigail", "Olivia", "Isabella", "Sophia", "Ava", "Charlotte", "Mia", "Amelia", "Evelyn", 
+"Abby", "Ellie", "Harper", "Emily", "Avery", "Ella", "Grace", "Sofia", "Chloe", "Zoe", "Victoria", "Aubrey", "Scarlett", "Hannah", 
+"Lily", "Aria", "Layla", "Riley", "Zoey", "Samantha", "Nora", "Leah", "Audrey", "Brooklyn", "Bella", "Violet", "Claire", "Savannah", 
+"Allison", "Sydney", "Skylar", "Camila", "Natalie", "Solange"]
+
+male_names = ["James", "John", "Robert", "Michael", "William", "David", "Richard", "Joseph", "Thomas", "Charles", "Christopher", "Daniel", 
+"Matthew", "George", "Donald", "Anthony", "Paul", "Mark", "Edward", "Steven", "Kenneth", "Andrew", "Brian", "Joshua", "Kevin", "Ronald",
+"Timothy", "Jason", "Jeffrey", "Frank", "Gary", "Ryan", "Nicholas", "Eric", "Stephen", "Jacob", "Larry", "Jonathan", "Scott", "Raymond", 
+"Justin", "Brandon", "Gregory", "Samuel", "Benjamin"]
+
+surnames = ["Martin", "Bernard", "Dubois", "Thomas", "Robert", "Richard", "Petit", "Durand", "Leroy", "Moreau", "Simon", "Laurent", 
+"Lefebvre", "Michel", "Garcia", "David", "Bertrand", "Roux", "Vincent", "Fournier", "Morel", "Girard", "Andre", "Leclerc", "Mercier", "Dupont", 
+"Lambert", "Bonnet", "Francois", "Martinez", "Legrand", "Garnier", "Faure", "Rousseau", "Blanc", "Guerin", "Muller", "Henry", "Roussel", "Nicolas", 
+"Perrin", "Morin", "Mathieu", "Clement"]
+
+# some tables don't specify sex
+first_names = female_names + male_names
+
+# fill the contact table
+
+# requires a phone number, so we create a function to generate a random french phone number
+# generate a random French phone number
+def generate_phone_number():
+    # french phone numbers have the format "01 XX XX XX XX"
+    return ("01 " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + " " + str(random.randint(0, 9)) + str(random.randint(0, 9)) + " " + 
+    str(random.randint(0, 9)) + str(random.randint(0, 9)))
+
+# we'll create 20 contacts and add to the contacts table
+for i in range(20):
+  # get necessary items for entry
+  first_name = random.choice(first_names)
+  last_name = random.choice(surnames)
+  phone_number = generate_phone_number()
+  # use the INSERT statement to add the activity data to the database
+  my_cursor.execute("INSERT IGNORE INTO ContactPerson (ContactName, ContactLastName, ContactPhoneNumber) VALUES (%s, %s, %s)", (first_name, last_name, phone_number))
+
+# fill the nurse table
+# we'll make 3 addresses for their professional addresses. we can think of this as there being 3 nurse agencies
+nurse_addresses = ['72 Rue des Arbres','302 Avenue de la Lune','93 Boulevard de la Mer']
+# we'll create 15 nurses and add to the contacts table
+for i in range(15):
+  # get necessary items for entry
+  first_name = random.choice(first_names)
+  last_name = random.choice(surnames)
+  phone_number = generate_phone_number()
+  prof_address = random.choice(nurse_addresses)
+  # use the INSERT statement to add the activity data to the database
+  my_cursor.execute("INSERT IGNORE INTO Nurse (Surname, Name, NurseAddress, PhoneNb) VALUES (%s, %s, %s, %s)", (last_name, first_name, prof_address, phone_number))
+
     
-  
+
 my_cursor.execute("SELECT * FROM SkillActivity")
+my_cursor.execute("SELECT * FROM ContactPerson")
+my_cursor.execute("SELECT * FROM Nurse")
 
 myresult = my_cursor.fetchall()
 
---
+++ table_creation_strings.py
@@ -0,0 +1,171 @@
+# contains all the mysql commands to create tables
+create_activity = """
+    CREATE TABLE IF NOT EXISTS SkillActivity(
+        ActivityName VARCHAR(50),
+        Difficulty INT,
+        Price DECIMAL(4,2),
+        PRIMARY KEY(ActivityName)
+    );
+"""
+
+create_contact = """
+    CREATE TABLE IF NOT EXISTS ContactPerson(
+      ContactId INT AUTO_INCREMENT PRIMARY KEY,
+      ContactName VARCHAR(50),
+      ContactLastName VARCHAR(50),
+      ContactPhoneNumber VARCHAR(50)
+    );
+"""
+
+create_nurse = """
+    CREATE TABLE IF NOT EXISTS Nurse(
+      NurseId INT AUTO_INCREMENT PRIMARY KEY,
+      Surname VARCHAR(50),
+      Name VARCHAR(50),
+      NurseAddress VARCHAR(50),
+      PhoneNb VARCHAR(50)
+    );
+"""
+
+create_medication = """
+    CREATE TABLE IF NOT EXISTS Medication(
+      NameM VARCHAR(50),
+      ActiveSubstance VARCHAR(50),
+      PRIMARY KEY(NameM)
+    );
+"""
+
+create_inhabitant = """
+    CREATE TABLE IF NOT EXISTS Inhabitant(
+      InHabitantId INT AUTO_INCREMENT PRIMARY KEY,
+      LastName VARCHAR(50),
+      FirstName VARCHAR(50),
+      Category CHAR(50),
+      Address VARCHAR(50),
+      PhoneNumber VARCHAR(50),
+      Gender BINARY(50),
+      NurseId_Follows INT,
+      ContactId INT,
+      FOREIGN KEY(NurseId_Follows) REFERENCES Nurse(NurseId),
+      FOREIGN KEY(ContactId) REFERENCES ContactPerson(ContactId)
+    );
+"""
+
+create_session = """
+    CREATE TABLE IF NOT EXISTS Session(
+      SessionId INT AUTO_INCREMENT PRIMARY KEY,
+      Dates DATE,
+      StartingTime TIME,
+      EndingTime TIME,
+      ActivityName VARCHAR(50) NOT NULL,
+      InHabitantId INT NOT NULL,
+      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
+      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
+    );
+"""
+
+create_userSN = """
+    CREATE TABLE IF NOT EXISTS UserSN(
+      LogIn VARCHAR(50),
+      Password VARCHAR(50),
+      Pseudo VARCHAR(50),
+      Email VARCHAR(50),
+      InHabitantId INT NOT NULL,
+      PRIMARY KEY(LogIn),
+      UNIQUE(InHabitantId),
+      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
+    );
+"""
+
+create_comments = """
+    CREATE TABLE IF NOT EXISTS Comments(
+      CommentID INT AUTO_INCREMENT PRIMARY KEY,
+      DateComment DATETIME,
+      Content VARCHAR(140),
+      CommentID_isCommentedBy INT,
+      LogIn VARCHAR(50) NOT NULL,
+      FOREIGN KEY(CommentID_isCommentedBy) REFERENCES Comments(CommentID),
+      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn)
+    );
+"""
+
+create_visit = """
+    CREATE TABLE IF NOT EXISTS Visit(
+      VisitId INT AUTO_INCREMENT PRIMARY KEY,
+      DateVisit DATETIME,
+      Duration INT,
+      Reason CHAR(50),
+      TextualRemark VARCHAR(200),
+      BloodPressureSystolic INT,
+      BloodPressureDiastolic INT,
+      SugarLevel INT,
+      InHabitantId INT NOT NULL,
+      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId)
+    );
+"""
+
+create_participation = """
+    CREATE TABLE IF NOT EXISTS Participation(
+      InHabitantId INT,
+      SessionId INT,
+      Rating INT,
+      PRIMARY KEY(InHabitantId, SessionId),
+      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId),
+      FOREIGN KEY(SessionId) REFERENCES Session(SessionId)
+    );
+"""
+
+create_declaring = """
+    CREATE TABLE IF NOT EXISTS Declaring(
+      ActivityName VARCHAR(50),
+      LogIn VARCHAR(50),
+      LevelD VARCHAR(50),
+      PRIMARY KEY(ActivityName, LogIn),
+      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
+      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn)
+    );
+"""
+create_recommending = """
+    CREATE TABLE IF NOT EXISTS Recommending(
+      ActivityName VARCHAR(50),
+      LogIn_is_recommended_by VARCHAR(50),
+      LogIn_recommends VARCHAR(50),
+      LevelR VARCHAR(50),
+      PRIMARY KEY(ActivityName, LogIn_is_recommended_by, LogIn_recommends),
+      FOREIGN KEY(ActivityName) REFERENCES SkillActivity(ActivityName),
+      FOREIGN KEY(LogIn_is_recommended_by) REFERENCES UserSN(LogIn),
+      FOREIGN KEY(LogIn_recommends) REFERENCES UserSN(LogIn)
+);
+"""
+
+create_following = """
+    CREATE TABLE IF NOT EXISTS Following(
+      LogIn_Is_Followed_by_ VARCHAR(50),
+      LogIn_Follows VARCHAR(50),
+      PRIMARY KEY(LogIn_Is_Followed_by_, LogIn_Follows),
+      FOREIGN KEY(LogIn_Is_Followed_by_) REFERENCES UserSN(LogIn),
+      FOREIGN KEY(LogIn_Follows) REFERENCES UserSN(LogIn)
+);
+"""
+
+create_liking = """
+    CREATE TABLE IF NOT EXISTS Liking(
+      LogIn VARCHAR(50),
+      CommentID INT,
+      PRIMARY KEY(LogIn, CommentID),
+      FOREIGN KEY(LogIn) REFERENCES UserSN(LogIn),
+      FOREIGN KEY(CommentID) REFERENCES Comments(CommentID)
+);
+"""
+
+create_prescribing = """
+    CREATE TABLE IF NOT EXISTS Prescribing(
+      InHabitantId INT,
+      NameM VARCHAR(50),
+      Startingtime DATE,
+      Duration INT,
+      PRIMARY KEY(InHabitantId, NameM),
+      FOREIGN KEY(InHabitantId) REFERENCES Inhabitant(InHabitantId),
+      FOREIGN KEY(NameM) REFERENCES Medication(NameM)
+);
+"""
